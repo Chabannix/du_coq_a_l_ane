910be4bf66ffe41d5dd28476645cd0ca64260d94
chabannix <chabannix@gmail.com>
2018-12-11 16:48:01 +0100

    Create graphe from dico

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "data.h"
 
-int main()
-{
-	printf("****  du coq a l'ane  ****\n");
 
-	// Process dictionnaries
-	FILE* file = fopen("dico.txt", "r");
+int dico2graph(char* dico, T_GRAPHE* Graphe)
+{
+	if(dico==NULL || Graphe == NULL)
+		return 0;
+
+	if(Graphe->graphe != NULL || Graphe->graphe != 0)
+		return 0;
+
+	// Open file
+	FILE* file = fopen(dico, "r");
 	if(file == NULL)
 	{
 		printf("\nError : impossible to open dictionnary file\n");
 		return 0;
 	}
+
+	// Extract words from file
 	char tab_words[100][50];    // 100 words of max 50 char
 	int dico_processing_over = 0;
 	
@@ -28,6 +36,29 @@ int main()
 			i++;
 		}
 	}
+	Graphe->taille = i;
+	Graphe->graphe = malloc(i*sizeof(T_SOMMET));
+
+	// Stock words into graphe
+	for(i=0; i<Graphe->taille; i++)
+	{
+		T_SOMMET sommet;
+		sommet.mot = tab_words[i];
+		sommet.Liste_succ = NULL;
+		Graphe->graphe[i] = sommet;
+	}
+	return 1;
+}
+
+int main()
+{
+	printf("****  du coq a l'ane  ****\n");
+
+	T_GRAPHE Graphe;
+	Graphe.graphe = NULL;
+	Graphe.taille = 0;
+
+	dico2graph("dico.txt", &Graphe);
 
 	printf("\nType the initial word\n");
 	char initial_word[4];
