010630cd8e1467a6045ec7428191ef1a67b495c0
chabannix <chabannix@gmail.com>
2018-12-11 15:27:15 +0100

    Creation of data.h

---
+++ data.h
@@ -0,0 +1,22 @@
+#ifndef DATA_H
+#define DATA_H
+
+struct T_SOMMET;
+
+typedef struct T_SOMMET* T_SUCC;
+typedef struct T_SOMMET  T_SOMMET;
+typedef struct lsucc*    L_SUCC;
+
+struct lsucc
+{
+    T_SUCC val;
+    struct lsucc* suiv;
+};
+
+struct T_SOMMET
+{
+    char* mot; 
+    L_SUCC Liste_succ;
+};
+
+#endif
