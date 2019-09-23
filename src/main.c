00fbd928a18ab62024e06df3c549da6791a6a3ee
chabannix <chabannix@gmail.com>
2019-09-23 18:07:38 +0200

    Loop in main program instead of quiting after first use

--- src/main.c
+++ src/main.c
@@ -43,43 +43,43 @@ int main(int argc, char *argv[])
     Graphe.taille = dicoInfo.wordsNumber;
     MEASURE_TIME( dico2graph(dicoFilename, &Graphe), "dico2graphe");
 
-    int OK = 0;
-    printf("\n-- Type the initial word\n"); fflush(stdout);
-    char* initial_word = malloc(Graphe.wordSize*sizeof(char));
+    while(1){
+        int OK = 0;
+        printf("\n-- Type the initial word\n"); fflush(stdout);
+        char* initial_word = malloc(Graphe.wordSize*sizeof(char));
 
-    while( !OK )
-    {
-        scanf("%s", initial_word);
-        if( !isWordInGraphe(initial_word, &Graphe) )
+        while( !OK )
         {
-            fprintf(stderr, "Error : the word is not in the dictionnary, type another word\n");
-            memset(initial_word, 0, Graphe.wordSize);
+            scanf("%s", initial_word);
+            if( !isWordInGraphe(initial_word, &Graphe) )
+            {
+                fprintf(stderr, "Error : the word is not in the dictionnary, type another word\n");
+                memset(initial_word, 0, Graphe.wordSize);
+            }
+            else
+            {
+                OK = 1;
+            }
         }
-        else
+
+        OK = 0;
+        printf("\n-- Type the final word\n");
+        char* final_word = malloc(Graphe.wordSize*sizeof(char));;
+        while( !OK )
         {
-            OK = 1;
+            scanf("%s", final_word);
+            if( !isWordInGraphe(final_word, &Graphe) )
+            {
+                fprintf(stderr, "Error : the word is not in the dictionnary, type another word\n");
+                memset(final_word, 0, Graphe.wordSize);
+            }
+            else
+            {
+                OK = 1;
+            }
         }
+        printf("\n");
+        MEASURE_TIME(dijkstraAlgo(&Graphe, initial_word, final_word), "dijkstraAlgo");
     }
-
-    OK = 0;
-    printf("\n-- Type the final word\n");
-    char* final_word = malloc(Graphe.wordSize*sizeof(char));;
-    while( !OK )
-    {
-        scanf("%s", final_word);
-        if( !isWordInGraphe(final_word, &Graphe) )
-        {
-            fprintf(stderr, "Error : the word is not in the dictionnary, type another word\n");
-            memset(final_word, 0, Graphe.wordSize);
-        }
-        else
-        {
-            OK = 1;
-        }
-    }
-    printf("\n");
-
-    MEASURE_TIME(dijkstraAlgo(&Graphe, initial_word, final_word), "dijkstraAlgo");
-
     return 0;
 }
